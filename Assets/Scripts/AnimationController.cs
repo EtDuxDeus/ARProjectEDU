821232f4e7a7c9e9127d87ca80c012cc0cdfa671
EtDuxDeus <minerukraine@gmail.com>
2021-06-16 12:52:58 +0300

    Fix (AnimationController) fixed AnimationController behavior -removed method ChangeAnimationState -added Pause and Continue Animtaion methods -changed formating settings from spaces to tabs -add buid named with codestandarts

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Tools
 {
-    public class AnimationController : MonoBehaviour
-    {
-        public Animator _charAnimator;
+	public class AnimationController : MonoBehaviour
+	{
+		[SerializeField]
+		private Animator _charAnimator;
 
-        private readonly int _hashOpenTrigger = Animator.StringToHash("isDancing");
+
+		public void PauseAnimation()
+		{
+			_charAnimator.speed = 0;
+		}
 
 
-        public void ChangeAnimationState()
-        {
-            bool isDancing = _charAnimator.GetBool("isDancing");
-
-            _charAnimator.SetBool(_hashOpenTrigger, !isDancing);
-        }
-
-    }
+		public void ContinueAnimation()
+		{
+			_charAnimator.speed = 1;
+		}
+	}
 }
--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -1,89 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 
 namespace Tools
 {
-    public class AudioPlayer : MonoBehaviour
-    {
-        [SerializeField]
-        private AudioSource _currentTrack;
-        [SerializeField]
-        private AudioClip[] _playlist;
-        private int _positionInPlaylist;
+	public class AudioPlayer : MonoBehaviour
+	{
+		[SerializeField]
+		private AudioSource _currentTrack;
+		[SerializeField]
+		private AudioClip[] _playlist;
+		private int _positionInPlaylist;
 
 
-        private void Awake()
-        {
-            _currentTrack = GetComponent<AudioSource>();
-            _positionInPlaylist = 0;
-            FirstStartMusic();
-        }
+		private void Awake()
+		{
+			_currentTrack = GetComponent<AudioSource>();
+			_positionInPlaylist = 0;
+			FirstStartMusic();
+		}
 
 
-        public void ResetMusic()
-        {
-            _positionInPlaylist = 0;
-            _currentTrack.clip = _playlist[_positionInPlaylist];
-            _currentTrack.Stop();
-        }
+		public void ResetMusic()
+		{
+			_positionInPlaylist = 0;
+			_currentTrack.clip = _playlist[_positionInPlaylist];
+			_currentTrack.Stop();
+		}
 
 
-        public void PlayNextTrack()
-        {
-            if (_positionInPlaylist < _playlist.Length - 1)
-            {
-                _positionInPlaylist++;
-            }
-            else
-            {
-                _positionInPlaylist = 0;
-            }
-            _currentTrack.clip = _playlist[_positionInPlaylist];
-            PlayMusic();
-        }
+		public void PlayNextTrack()
+		{
+			if (_positionInPlaylist < _playlist.Length - 1)
+			{
+				_positionInPlaylist++;
+			}
+			else
+			{
+				_positionInPlaylist = 0;
+			}
+			_currentTrack.clip = _playlist[_positionInPlaylist];
+			PlayMusic();
+		}
 
 
-        public void PlayPreviousTrack()
-        {
-            if (_positionInPlaylist == 0)
-            {
-                _positionInPlaylist = _playlist.Length - 1;
-            }
-            else
-            {
-                _positionInPlaylist--;
-            }
-            _currentTrack.clip = _playlist[_positionInPlaylist];
-            PlayMusic();
-        }
+		public void PlayPreviousTrack()
+		{
+			if (_positionInPlaylist == 0)
+			{
+				_positionInPlaylist = _playlist.Length - 1;
+			}
+			else
+			{
+				_positionInPlaylist--;
+			}
+			_currentTrack.clip = _playlist[_positionInPlaylist];
+			PlayMusic();
+		}
 
 
-        public void PauseMusic()
-        {
-            _currentTrack.Pause();
-        }
+		public void PauseMusic()
+		{
+			_currentTrack.Pause();
+		}
 
 
-        public void UnPauseMusic()
-        {
-            _currentTrack.UnPause();
+		public void UnPauseMusic()
+		{
+			_currentTrack.UnPause();
 
-        }
-
-
-        public void PlayMusic()
-        {
-            _currentTrack.Play();
-        }
+		}
 
 
-        public void FirstStartMusic()
-        {
-            _currentTrack.clip = _playlist[_positionInPlaylist];
-            PlayMusic();
-        }
-    }
+		public void PlayMusic()
+		{
+			_currentTrack.Play();
+		}
+
+
+		public void FirstStartMusic()
+		{
+			_currentTrack.clip = _playlist[_positionInPlaylist];
+			PlayMusic();
+		}
+	}
 }
